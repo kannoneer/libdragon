b6b5ccc990e202610eefa20e6d1be902c61fb15c
tykkiman <tykkimies@protonmail.com>
2023-12-15 18:19:13 +0200

    add tests assets

--- examples/occlusion/convert_assets.sh
+++ examples/occlusion/convert_assets.sh
@@ -2,3 +2,4 @@
 set -ex
 MKMODEL=../../tools/mkmodel/mkmodel
 $MKMODEL -o filesystem -v  '/home/user/Documents/blender/rooms/room1.glb'
+#$MKMODEL -o filesystem -v  '/home/user/Documents/blender/rooms/icosphere.glb'
