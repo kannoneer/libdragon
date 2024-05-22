3597e76c86e13e1e11a060dc5f3592aaff73227f
tykkiman <tykkimies@protonmail.com>
2024-05-22 22:25:48 +0300

    animations

--- examples/gameloop/gameloop.c
+++ examples/gameloop/gameloop.c
@@ -3,13 +3,13 @@
 #include <float.h>
 #include "n64sys.h"
 
-#define PLAYER_NUM_FRAMES (12)
-#define PLAYER_SPRITE_SIZE (32)
+// Player spritesheet constants
+#define PLAYER_NUM_FRAMES (12)              // Total number of frames in the spritesheet
+#define PLAYER_SPRITE_SIZE (32)             // 32x32 pixel frames
+#define PLAYER_ANIM_FRAMES_PER_SECOND (20)  // Player animation speed
 
-static sprite_t *player_sprite;
-static sprite_t *apple_sprite;
-
-static wav64_t sfx_hit;
+// Mixer channel allocation, we have just one for now
+#define CHANNEL_SFX (0)
 
 enum {
     FONT_PACIFICO = 1,
@@ -25,22 +25,27 @@ enum {
     FACING_LEFT = 1,
 };
 
+// Sprites
+static sprite_t *player_sprite;
+static sprite_t *apple_sprite;
+
+// Sound effects
+static wav64_t sfx_hit;
+
+// Keep game object state in their own anonymous structs.
+// This way their variables are kept in their own namespaces.
+
 static struct {
     vector2d_t pos;
+    float drop;
 } apple;
 
 static struct {
     vector2d_t pos;
     int facing;
     int frame;
-    int anim_counter;
+    float anim_counter;
 } player;
-
-// Mixer channel allocation
-#define CHANNEL_SFX1    0
-#define CHANNEL_SFX2    2
-#define CHANNEL_SFX3    4
-#define CHANNEL_MUSIC   6
 
 static float distance_squared(vector2d_t *a, vector2d_t *b)
 {
@@ -49,7 +54,7 @@ static float distance_squared(vector2d_t *a, vector2d_t *b)
     return dx*dx + dy*dy;
 }
 
-void update(float deltatime)
+void update(float dt)
 {
     joypad_poll();
     joypad_buttons_t buttons = joypad_get_buttons(JOYPAD_PORT_1);
@@ -57,27 +62,41 @@ void update(float deltatime)
     const float player_speed = 150.0f; // Unit is pixels per second
 
     if (buttons.d_left) {
-        player.pos.x -= deltatime * player_speed;
+        player.pos.x -= dt * player_speed;
         player.facing = FACING_LEFT;
     }
     if (buttons.d_right) {
-        player.pos.x += deltatime * player_speed;
+        player.pos.x += dt * player_speed;
         player.facing = FACING_RIGHT;
     }
     if (buttons.d_up) {
-        player.pos.y -= deltatime * player_speed;
+        player.pos.y -= dt * player_speed;
     }
     if (buttons.d_down) {
-        player.pos.y += deltatime * player_speed;
+        player.pos.y += dt * player_speed;
     }
 
-    const int hit_radius = 25;
+    const int hit_radius = 20;
 
     if (distance_squared(&player.pos, &apple.pos) < hit_radius * hit_radius) {
-        wav64_play(&sfx_hit, CHANNEL_SFX1);
+        wav64_play(&sfx_hit, CHANNEL_SFX);
         const int margin = 20;
-        apple.pos.x = margin + rand() % (display_get_width() - 2*margin);
-        apple.pos.y = margin + rand() % (display_get_height() - 2*margin);
+        apple.pos.x = margin + rand() % (display_get_width() - 2 * margin);
+        apple.pos.y = margin + rand() % (display_get_height() - 2 * margin);
+        apple.drop = 6.0f;
+    }
+
+    // Update animations
+
+    player.anim_counter += dt;
+    if (player.anim_counter > 1.0f/PLAYER_ANIM_FRAMES_PER_SECOND) {
+        player.frame = (player.frame + 1) % PLAYER_NUM_FRAMES;
+        player.anim_counter = 0.0f;
+    }
+
+    apple.drop -= dt * 150.0f;
+    if (apple.drop < 0.0f) {
+        apple.drop = 0.0;
     }
 }
 
@@ -87,19 +106,12 @@ void render()
     rdpq_attach_clear(disp, NULL);
 
     rdpq_set_mode_standard();
-    rdpq_sprite_blit(apple_sprite, apple.pos.x, apple.pos.y, &(rdpq_blitparms_t){});
+    rdpq_sprite_blit(apple_sprite, apple.pos.x, apple.pos.y - apple.drop, NULL);
     rdpq_sprite_blit(player_sprite, player.pos.x, player.pos.y, &(rdpq_blitparms_t){
         .s0 = player.frame * PLAYER_SPRITE_SIZE,
         .width = PLAYER_SPRITE_SIZE,
-        .flip_x = player.facing == FACING_LEFT
+        // .flip_x = player.facing == FACING_LEFT
     });
-
-    player.anim_counter++;
-    if (player.anim_counter > 5) {
-        player.frame = (player.frame + 1) % PLAYER_NUM_FRAMES;
-        player.anim_counter = 0;
-    }
-
 
     rdpq_detach_show();
 }
@@ -109,20 +121,17 @@ int main()
     debug_init_isviewer();
     debug_init_usblog();
 
-    display_init(RESOLUTION_320x240, DEPTH_16_BPP, 3, GAMMA_NONE, FILTERS_RESAMPLE_ANTIALIAS_DEDITHER);
+    display_init(RESOLUTION_320x240, DEPTH_16_BPP, 3, GAMMA_NONE, FILTERS_RESAMPLE);
 
     joypad_init();
     timer_init();
 
-    uint32_t display_width = display_get_width();
-    uint32_t display_height = display_get_height();
-    
     dfs_init(DFS_DEFAULT_LOCATION);
 
     rdpq_init();
 
 	audio_init(44100, 4);
-	mixer_init(20);
+	mixer_init(10); // The number of concurrently playing channels
 
 	wav64_open(&sfx_hit, "rom:/blip.wav64");
 
